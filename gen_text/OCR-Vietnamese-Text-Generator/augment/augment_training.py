f0a49d584273a799e2efc867dce26475d3084f21
sonnt373 <songuyen222k>
2023-10-23 10:23:04 +0700

    update data augment

--- gen_text/OCR-Vietnamese-Text-Generator/augment/augment_training.py
+++ gen_text/OCR-Vietnamese-Text-Generator/augment/augment_training.py
@@ -6,6 +6,7 @@ import os
 import random
 from tqdm import tqdm
 import concurrent.futures
+import argparse
 
 def save_dict2txt(results_dict, out_path):
     with open(out_path, "a", encoding="utf-8") as outfile:  # Use "a" mode for appending
@@ -75,81 +76,88 @@ def augmentation(image):
         # cv2.imwrite("check.jpg", image)
     image = cv2.resize(image, (int(image.shape[1] * 64 / image.shape[0]), 64 ))    # 48
 
-
-
     transform = A.Compose([
     A.Blur(p = 0.1),
     A.MotionBlur(p = 0.1),
-    # A.ElasticTransform(alpha=1, alpha_affine = 3, p =0.2),
     A.GaussNoise(p = 0.1),
     A.GridDistortion( num_steps=5, distort_limit=0.1, p = 0.2),
     A.CLAHE(p = 0.2),
     A.RandomBrightnessContrast(p=0.7),
     A.ISONoise(p = 0.3),
     A.RGBShift(p = 0.5),
-    # A.ShiftScaleRotate(shift_limit=0.02, scale_limit=0.05, rotate_limit=7, p=0.2),
+    A.ChannelShuffle(p = 0.5),
+    A.ShiftScaleRotate( shift_limit = 0.0, shift_limit_y = 0.02, scale_limit=0.0, rotate_limit=1, p=0.2),
     # A.ElasticTransform(alpha_affine=0.5, alpha=0.5, sigma=0, p = 0.1),
     # A.Perspective(p = 0.2),
     A.ToGray(p = 0.25),
     ])
         
     image_aug_list = []
-    for _ in range(1):
+    for _ in range(10):
+
+        random_crop =  random.uniform(0,1)
+        if random_crop > 0.6:
+        # Define the desired height and width for the cropped image
+            desired_height = image.shape[0]  # Keep the original height
+            desired_width = int(image.shape[1] * random.uniform(0.97,1))  # Crop random % from the right
+
+            # # Randomly choose the top-left corner for cropping
+            # y_start = np.random.randint(0, image.shape[0] - desired_height + 1)
+            # x_start = np.random.randint(0, image.shape[1] - desired_width + 1)
+
+            # Crop the image
+            image = image[0 : desired_height, 0 : 0 + desired_width]
+
         transformed = transform(image=image)
         transformed_image = transformed['image']
-
-        # aug_1 = random.uniform(0,1)
-        # if aug_1 > 0.5:
-        # # preprocess image line text
-        #     transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2GRAY)
 
         image_aug_list.append(transformed_image)
     return image_aug_list
 
 
-def process_line(line):
-    label_text = line.split('\t')[1]
-    name_file_img = line.split('\t')[0]
-    img_path_input = os.path.join(root, name_file_img)
+def process_line(img_path_input):
     img = cv2.imread(img_path_input)
-    
-
-    # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
-
-
-    # kernel = np.ones((2,2), np.uint8)  # Define kernel for erosion and dilation, adjust size as needed
-    # img = cv2.dilate(img, kernel, iterations=1)  # Erosion
-    # img = cv2.erode(img, kernel, iterations=random.randint(3, 5))  # Erosion
-
-    # image = np.expand_dims(img , axis = 2)
-    # image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
     image_aug_list = augmentation(img)
-    result_image_dict = {}
 
-    for image_aug in image_aug_list:
-        name_save = name_file_img.split('/')[-1]
-        result_image_dict[os.path.join("data_ocr_v1_0210_aug", name_save)] = label_text
-        cv2.imwrite(os.path.join(folder_output, name_save), image_aug)
-
-    return result_image_dict
+    return image_aug_list
 
 # Your input and output paths and other variables
-root = "/home/sonlt373/Desktop/SoNg/OCR_handwriting_shop_sticker/data/data_sticker_handwriting/KALAPA_ByteBattles_2023_OCR_Set1/OCR/training_HWT_v1_171023/"
-input_path = "/home/sonlt373/Desktop/SoNg/OCR_handwriting_shop_sticker/data/data_sticker_handwriting/KALAPA_ByteBattles_2023_OCR_Set1/OCR/training_HWT_v1_171023/data_hwt_500k_syn.txt"
-folder_output = "/home/sonlt373/Desktop/SoNg/OCR_handwriting_shop_sticker/data/data_sticker_handwriting/KALAPA_ByteBattles_2023_OCR_Set1/OCR/training_HWT_v1_171023/data_500k_hwt_synthetic_augment"
+# root = "/home/sonlt373/Desktop/SoNg/OCR_handwriting_shop_sticker/data/data_sticker_handwriting/KALAPA_ByteBattles_2023_OCR_Set1/OCR/kalapa_training_set/kalapa_train_fixed/"
+# input_path = os.path.join(root, "images")
+# anno_input_path = os.path.join(root, "labels")
+# Function to process a single image
+def process_image(root, image_path, name, annotation):
+    input_path = os.path.join(root, "images")
+    anno_input_path = os.path.join(root, "labels")
+    image_aug_list = process_line(image_path)
 
-result_image_list = {}
+    for i, image_aug in enumerate(image_aug_list):
+        image_path_out = os.path.join(input_path, name + f'_aug_{i}.jpg')
+        anno_path_out = os.path.join(anno_input_path, name + f'_aug_{i}.txt')
+        cv2.imwrite(image_path_out, image_aug)
+        with open(anno_path_out, 'w') as f:
+            f.write(annotation)
 
-# Use a ThreadPoolExecutor to parallelize the processing
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    with open(input_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        results = list(tqdm(executor.map(process_line, lines[:-1]), total=len(lines[:-1])))
+def main(root):
+    input_path = os.path.join(root, "images")
+    anno_input_path = os.path.join(root, "labels")
 
-    # Combine results from all threads into a single dictionary
-    # for result in results:
-    #     result_image_list.update(result)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for path_anno in tqdm(os.listdir(anno_input_path)):
+            name = path_anno[:-4]
+            anno_path = os.path.join(anno_input_path, path_anno)
+            image_path = os.path.join(input_path, name + '.jpg')
 
-# Save the combined dictionary to a file
-# save_dict2txt(result_image_list, "label_0210_aug.txt")
+            with open(anno_path, 'r') as f:
+                annotation = f.read()
+
+            executor.submit(process_image, root, image_path, name, annotation)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Image processing script")
+    parser.add_argument("--root", required=True, default= '/home/sonlt373/Desktop/SoNg/OCR_handwriting_shop_sticker/data/data_sticker_handwriting/KALAPA_ByteBattles_2023_OCR_Set1/OCR/kalapa_training_set/kalapa_train_fixed/', help="Root path for input and output")
+    args = parser.parse_args()
+    
+    main(args.root)
+    print("-----------------------------------------------------------")
